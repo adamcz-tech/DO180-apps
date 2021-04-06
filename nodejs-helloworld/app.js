59dfa213697fdd2e0286737871e77f9406b09351
ac <adam.czerminski@pl.ibm.com>
2021-04-06 17:43:13 +0200

    1.4

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World ar!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+var port = 8081
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
