7cf1de423b5a836a9bf74e4c888626921b172576
ac <adam.czerminski@pl.ibm.com>
2021-04-08 18:53:04 +0200

    env

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 app = express();
 
-var port = 8081
+var port = 8081;
 var requestNo = 0;
 
 app.get('/', function (req, res) {
 	requestNo++;
-  res.send(`Hello World ar at port ${port} - request no = ${requestNo}\n`);
+  res.send(`Hello World ar at port ${port} - request no = ${requestNo}, A=${process.env.A}, INTERNAL=${process.env.INTERNAL}\n`);
 });
 
 app.listen(port, function () {
